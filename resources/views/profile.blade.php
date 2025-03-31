fa92608e65ef3d443a488a2e74c8afd9b03cec56
Shaldi Shauqi <shaldishauqi2016@gmail.com>
2025-03-31 18:37:38 +0700

    Update main profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -29,7 +29,57 @@
 
     <!-- Main -->
     <main>
-        
+        <div class="bg-[#FFFBF6] rounded-3xl shadow-lg p-8 w-full max-w-md mx-4">
+          
+          <!-- Profile Header -->
+          <div class="flex items-center gap-4 bg-gray-50 rounded-lg p-4 mb-8 border">
+            <div class="w-12 h-12 bg-gray-200 rounded-full flex items-center justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+              </svg>
+            </div>
+            <div>
+              <h2 class="font-medium">John Doe</h2>
+              <p class="text-sm text-gray-500">john.doe12@gmail.com</p>
+            </div>
+          </div>
+          
+          <!-- Profile Fields -->
+          <div class="space-y-4">
+            <div class="flex justify-between items-center border-b border-gray-300 pb-2">
+              <span class="text-sm text-gray-600">Full name</span>
+              <span class="text-gray-900">John Doe</span>
+            </div>
+    
+            <div class="flex justify-between items-center border-b border-gray-300 pb-2">
+              <span class="text-sm text-gray-600">Username</span>
+              <span class="text-gray-900">john_doe12</span>
+            </div>
+    
+            <div class="flex justify-between items-center border-b border-gray-300 pb-2">
+              <span class="text-sm text-gray-600">Email</span>
+              <span class="text-gray-900">john.doe12@gmail.com</span>
+            </div>
+    
+            <div class="flex justify-between items-center border-b border-gray-300 pb-2">
+              <span class="text-sm text-gray-600">Password</span>
+              <span class="text-gray-900">********</span>
+            </div>
+          </div>
+    
+          <!-- Action Buttons -->
+          <div class="space-y-4">
+            <div class="flex gap-4 mt-6">
+              <button type="button" class="w-full bg-black text-white py-3 rounded-lg font-medium">Edit</button>
+              <button type="button" class="w-full bg-black text-white py-3 rounded-lg font-medium">Share</button>
+            </div>
+    
+            <button type="button" onclick="window.location.href='./index.html'" 
+              class="w-full text-[#FF1600] border border-[#FF988E] py-3 rounded-lg font-medium">
+              Sign out
+            </button>
+          </div> 
+        </div>
     </main>
 
     <!-- Footer -->
